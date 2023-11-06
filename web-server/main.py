4513ca67275913d87772945d587d15d1963d8e13
Mateo Vasco <macv9620@utp.edu.co>
2023-11-06 08:48:49 -0500

    Se crea server en FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,21 @@
-import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-def run():
-    store.get_categories()
+app = FastAPI()
 
-if __name__ == '__main__':
-    run()
+@app.get('/home')
+def get_list():
+    return {"message": "First FastAPI server!"}
+
+@app.get("/items/", response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
